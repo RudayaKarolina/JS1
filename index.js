0a51ebe067799aaf1af6fb5933f4ad2df3904968
RudayaKarolina <153662311+RudayaKarolina@users.noreply.github.com>
2024-01-03 19:12:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+//Задание 1
+// const user=prompt("Введите ваше имя");
+// alert("Привет, " + user + "!");
+
+//Задание 2
+// const num1=prompt("Введите число");
+// const num2=prompt("Введите степень, в которую нужно возвести число");
+// console.log(num1**num2);
+
+//Задание 4
+// let a='my text';
+// if (a=='some text') {
+//     a='another text'}
+// else 
+// {a='some text'}
+// console.log(a)
+
+// Задание 5
+// let b=10;
+// if(b==0){
+// b=1
+// }
+// else if(b<0){
+//     b='less then zero'
+// } else{
+//     b*=10
+// }
+// console.log(b)
+
+// Задание 6
+// let c=prompt('Введите число');
+// Number(c);
+// let n;
+// if (c < 5){
+//     n=0
+// }else{
+//     n=1
+// }
+// console.log(n)
+
+// Задание 7
+// const numb1=prompt('Введите первое число');
+// const numb2=prompt('Введите второе число');
+// if (numb1 > numb2){
+//     console.log('Большее из чисел: ' + numb1)
+// } else if (numb1==numb2){
+// console.log('Числа одинаковы')}
+// else{
+//     console.log('Большее из чисел: '+ numb2)
+// }
+
+//Задание 8
+// const num11=prompt('Введите первое число');
+// const num22=prompt('Введите второе число');
+// const num33=num11%num22;
+// if (num33==0){
+//     console.log("Первое число кратно второму")
+// }else{
+//     console.log("Первое число не кратно второму")
+// }
+
+
+//Задание 9
+// const ball=prompt("Введите Ваш средний балл");
+// if (ball >= 1&& ball <= 4){
+//         console.log("Двоечник, иди учись!")
+//     }else if(ball >= 5 && ball <= 8){
+//         console.log("Неплохо, но давай еще поднажмем!")
+//     }else if(ball >= 9 && ball <=1 0){
+//         console.log("Ого, да ты настоящий отличник!")
+//     }else{
+//         console.log("Такого не может быть")
+//     }
+
+//задание 10
+// const ekz=prompt('Введите балл за экзамен');
+// const pro=prompt('Введите количество выполненных проектов');
+// if(ekz > 90 || pro > 10){
+//     console.log('100')
+// }else if(ekz > 75 && pro >=5){
+//     console.log('90')
+// }else if (ekz > 50 && pro >= 2){
+//     console.log('75')
+// }else{
+//     console.log('0')
+// }
+
+// Задание 11
+// const day = prompt("Введите количество дней");
+// let cost=40*day;
+// if(day >=7){
+//     cost=cost-50
+// }else if(day >=3){
+//     cost=cost-20
+// }
+// console.log(cost)
+
